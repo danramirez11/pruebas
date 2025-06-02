1570280b889180e59314185e8e1fbb891407a744
Dan <125272254+danramirez11@users.noreply.github.com>
2025-06-01 20:56:09 -0500

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import {RouterProvider} from'react-router-dom'
 import routes from './Routes/Navigation.tsx'
 import './index.css';
+import './App.css';
 
 createRoot(document.getElementById('root')!).render(
   
