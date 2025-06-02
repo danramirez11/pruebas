e00c4b1fa79b51e2787c25556dce2b3d8d94af69
Dan <125272254+danramirez11@users.noreply.github.com>
2025-06-01 20:31:10 -0500

    Remove Tailwind and Set Up Regular CSS

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -1,9 +1,6 @@
-"use client";
-
+import React from 'react';
 import * as AvatarPrimitive from "@radix-ui/react-avatar";
-import * as React from "react";
-
-import { cn } from "../../lib/utils";
+import './avatar.css';
 
 const Avatar = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Root>,
@@ -11,10 +8,7 @@ const Avatar = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AvatarPrimitive.Root
     ref={ref}
-    className={cn(
-      "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
-      className,
-    )}
+    className={`avatar ${className || ''}`}
     {...props}
   />
 ));
@@ -26,7 +20,7 @@ const AvatarImage = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AvatarPrimitive.Image
     ref={ref}
-    className={cn("aspect-square h-full w-full", className)}
+    className={`avatar-image ${className || ''}`}
     {...props}
   />
 ));
@@ -38,10 +32,7 @@ const AvatarFallback = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AvatarPrimitive.Fallback
     ref={ref}
-    className={cn(
-      "flex h-full w-full items-center justify-center rounded-full bg-muted",
-      className,
-    )}
+    className={`avatar-fallback ${className || ''}`}
     {...props}
   />
 ));
