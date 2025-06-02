9aaca31870c101a7ff622f80233d642342703415
Dan <125272254+danramirez11@users.noreply.github.com>
2025-06-01 20:55:59 -0500

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import { createRoot } from 'react-dom/client'
 import {RouterProvider} from'react-router-dom'
 import routes from './Routes/Navigation.tsx'
-import './tailwind.css';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   
